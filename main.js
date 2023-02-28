5ed2f1be2736c606b3e9087794c0474b18235928
Tee <122542713+iamteewmcj@users.noreply.github.com>
2023-02-28 19:11:16 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+//Problem 6
+function squares(a, b){
+    console.log(a**2+b**2)
+}
+
+squares(5, 6)
+
+//Problem 11
+function even_or_odd(a){
+    if (a % 2 == 0){
+        console.log("even")
+    }else{
+        console.log("odd")
+    }
+}
+
+even_or_odd(4)
+even_or_odd(7)
+
+//Problem 3
+let pi = 3.14
+function area_of_circle(r){
+    console.log(pi*r**2)
+}
+
+area_of_circle(8)
